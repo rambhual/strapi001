b8913128de8eb964478e34121f6cf5cd8bc19d02
ramprits <rampritsahani@outlook.in>
2020-07-24 21:01:51 +0530

    first commit

---
+++ config/functions/cron.js
@@ -0,0 +1,28 @@
+"use strict";
+
+/**
+ * Cron config that gives you an opportunity
+ * to run scheduled jobs.
+ *
+ * The cron format consists of:
+ * [SECOND (optional)] [MINUTE] [HOUR] [DAY OF MONTH] [MONTH OF YEAR] [DAY OF WEEK]
+ *
+ * See more details here: https://strapi.io/documentation/v3.x/concepts/configurations.html#cron-tasks
+ */
+
+module.exports = {
+  "* * * * * *": async () => {
+    console.log("Cron every second");
+
+    const send = await strapi.plugins["email"].services.email.send({
+      to: "aeyijqxusvloeorazf@awdrt.com",
+      from: "demo@strapi.io",
+      replyTo: "demo@strapi.io",
+      subject: "Sent from a cron",
+      text: "Hello world!",
+      html: "Hello world!",
+    });
+
+    console.log("send", send);
+  },
+};
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5b08791f-5b6e-434b-91c4-0812176a28d5'
+};
